fd47c47f7c27cc0bbceb8a87855cfe6f102436dc
Tato98 <46031414+Tato98@users.noreply.github.com>
2019-05-25 20:34:23 +0200

    Modifica1
    Ho aggiunto la funzione data dalla somma tra l'onda incidente e quella riflessa e aggiustato di conseguenza l'output del programma(legenda e colori vari). Inoltre ho aggiunto un controllo sull'ampiezza della line di demarcazione tra i due dielettrici in modo tale che essa prenda, tramite l'if nella linea 32, il valore dell'ampiezza dell'onda più grande. Infine ho diminuito i "passi" del ciclo for per apprezzare al meglio l'andamento delle onde graficate (provare con i valori dielettrico1=9 e dielettrico2=1 e notare l'andamento della sinusoide verde).

--- progetto_elettromagnetismo.m
+++ progetto_elettromagnetismo.m
@@ -8,10 +8,11 @@ Mur2=1;
 Mu0=4*pi*10^(-7);
 Eps0=8.85418781762*10^(-12);
 f=300*10^6;
+z=1;
+amp=0;
 Epsr1= input("Inserire un valore per la costante dielettrica relativa del mezzo 1: ");
 Epsr2= input("Inserire un valore per la costante dielettrica relativa del mezzo 2: ");
 Ex= input("Inserire un ampiezza per l'onda incidente: ");
-z= input("Inserire un valore z per la fase dell'onda incidente: ");
 
 %coefficiente di riflessione
 p= (sqrt(Epsr1) - sqrt(Epsr2))/(sqrt(Epsr1) + sqrt(Epsr2));
@@ -25,17 +26,28 @@ B2=w*sqrt(Mu0*Eps0*Epsr2);
 
 %plot
 t= linspace(-10,0,1000);
-x=linspace(0,10,1000);
+x= linspace(0,10,1000);
 axis([-10 10 -10 10])
+%controllo per l'ampiezza della linea di demarcazione fra i due dielettrici
+if Ex > Ex*tao
+    amp = Ex;
+elseif Ex < Ex*tao
+    amp = Ex*tao;
+end   
 
-for i= 0 : 0.5 : 1000
+for i= 0 : 0.2 : 1000
+    %onda incidente
     Ei= Ex*cos(w*t -(i*pi + B1*z));
+    %onda riflessa
     Er= Ex*p*cos(w*t +(i*pi + B1*z));
+    %onda trasmessa
     Et= Ex*tao*cos(w*x -(i*pi + B2*z));
-    plot(t,Ei,t,Er,'--',x,Et,'linewidth',2);
-    line([0 0],[-Ex Ex],'linewidth',2,'Color','k');
+    %somma onda incidente e onda riflessa
+    E1= Ex*(cos(w*t -(i*pi + B1*z) + p*cos(w*t +(i*pi + B1*z))));
+    plot(t,Ei,'b',t,Er,'r--',t,E1,'g',x,Et,'y','linewidth',2);
+    line([0 0],[-amp amp],'linewidth',2,'Color','k');
     title('Incidenza di onde piane uniformi su di una superficie di separazione tra due dielettrici');
-    legend('Onda incidente','Onda riflessa','Onda trasmessa');
+    legend('Onda incidente','Onda riflessa','Somma onda incidente e onda riflessa','Onda trasmessa');
     grid on
     drawnow
 end
